6f969ac6fec41799a276656343191058988f5dc5
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-06-04 18:43:01 +0600

    V. Replace word - string practice sheet

--- V_Replace_Word.cpp
+++ V_Replace_Word.cpp
@@ -39,6 +39,30 @@ int main()
     return 0;
 }
 
+/*
+||-----------------------||
+||  Asif Mohammed Sifat  ||
+||  Department of CSE    ||
+||   CS Instructor       ||
+||Phitron,ProgrammingHero||
+||-----------------------||
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string str;
+    getline(cin,str);
+
+    while(str.find("EGYPT") != -1){
+        str.replace(str.find("EGYPT"), 5," ");
+    }        
+
+    cout<<str<<endl;
+              
+    return 0;
+}
+
+
 
 
 // #include <bits/stdc++.h>
